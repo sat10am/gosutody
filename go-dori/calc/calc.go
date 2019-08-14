425419728e7d395fdd3645e66903daa8f6e0f41c
godori <gej.okpo@gmail.com>
2019-08-15 02:00:27 +0900

    Home Assignment: Create a  Simple Calculator Operations

---
+++ go-dori/calc/calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add(a int, b int) int {
+	return a + b
+}
+
+func Subtract(a int, b int) int {
+	return a - b
+}
+
+func Multiply(a int, b int) int {
+	return a * b
+}
+
+func Devide(a int, b int) int {
+	return a / b
+}
